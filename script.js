9f3291506c08875053b2b74fb1a80fce96a325dc
nsri111 <nobleshivapuri111@gmail.com>
2025-08-21 14:14:49 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('footerReportsLink').addEventListener('click', () => {
+  document.getElementById('reportsModal').style.display = 'block';
+});
+
+document.getElementById('footerContactLink').addEventListener('click', () => {
+  document.getElementById('contactModal').style.display = 'block';
+});
